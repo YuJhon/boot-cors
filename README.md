bb8fd7faadbae0a2ae9280a4b83eaf0c39d86a7f
YuJhon <3291371805@qq.com>
2017-07-03 10:36:34 +0800

    SpringBoot CORS配置和Readme文件的完善

--- README.md
+++ README.md
@@ -1,4 +1,19 @@
 # boot-cors
-Spring Boot 使用CORS解决跨域问题
+    Spring Boot 使用CORS解决跨域问题示例
 
-未完成，待更新，敬请期待。。。
+### 服务器端的CORS配置
+![服务器端的CORS配置](http://jiangblog.oss-cn-shanghai.aliyuncs.com/boot-cors/server-code-project.png)
+
+### 客户端AJAX示例
+![客户端AJAX示例](http://jiangblog.oss-cn-shanghai.aliyuncs.com/boot-cors/Request-page.png)
+
+### 正确访问结果
+![正确访问结果](http://jiangblog.oss-cn-shanghai.aliyuncs.com/boot-cors/CROS-Result-Of-Header.png)
+
+### 错误访问结果
+![修改允许访问的源](http://jiangblog.oss-cn-shanghai.aliyuncs.com/boot-cors/modified-allow-origins.png)
+
+![错误访问结果](http://jiangblog.oss-cn-shanghai.aliyuncs.com/boot-cors/client-error.png)
+
+### 注意：
+    CORS是在Spring 4.2之后才能用CORSOrigin注解
---
+++ src/main/java/com/jhon/rain/config/CORSConfiguration.java
@@ -0,0 +1,34 @@
+package com.jhon.rain.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+/**
+ * <p>功能描述</br> TODO</p>
+ *
+ * @author jiangy19
+ * @version v1.0
+ * @FileName CORSConfiguration
+ * @date 2017/7/3 9:28
+ */
+@Configuration
+public class CORSConfiguration {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurerAdapter() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/cors/**")
+                        .allowedOrigins("http://localhost:8077")
+                        .allowedMethods("PUT", "DELETE","GET")
+                        .allowedHeaders("header1", "header2", "header3")
+                        .exposedHeaders("header1", "header2")
+                        .allowCredentials(false)
+                        .maxAge(3600);
+            }
+        };
+    }
+}
